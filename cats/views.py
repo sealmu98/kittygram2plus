fb67780f5157033d7e71da976b09e4b8545da76b
sealmu <seal.mu98@gmail.com>
2024-10-06 14:19:06 +0300

    filters

--- cats/views.py
+++ cats/views.py
@@ -1,21 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.throttling import ScopedRateThrottle
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Achievement, Cat, User
 from .permissions import OwnerOrReadOnly, ReadOnly
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
-from .throttling import WorkingHoursRateThrottle
+# from .throttling import WorkingHoursRateThrottle
+from rest_framework import filters
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
     permission_classes = (OwnerOrReadOnly,)
-    # Если кастомный тротлинг-класс вернёт True - запросы будут обработаны
-    # Если он вернёт False - все запросы будут отклонены
-    throttle_classes = (WorkingHoursRateThrottle, ScopedRateThrottle)
-    # А далее применится лимит low_request
-    throttle_scope = 'low_request'
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    pagination_class = None
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
--- kittygram2plus/settings.py
+++ kittygram2plus/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'djoser',
+    'django_filters',
     'cats.apps.CatsConfig',
 ]
 
